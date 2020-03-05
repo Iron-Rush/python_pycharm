5aa93c1d6e01fab1af4df8c0154decca45d845e9
wolf <chenzilin97@gmail.com>
2020-03-05 16:57:27 +0800

    Initial commit

---
+++ day1/Temperature.py
@@ -0,0 +1,25 @@
+# 华氏度-摄氏度 转换公式
+# C = (F-32)/1.8
+# F = C*1.8+32
+# C为摄氏度 F为华氏度
+# 回声程序
+print(input("请输入:"))
+
+print("你好啊"\
+      "我换行了")
+print("字符串"[1] in "符")
+
+A = {"python", 123, ("python", 121)}
+print(A.pop())
+print(A.pop())
+print(A.pop())
+
+# TempStr = input("请输入带有符号的温度值：")
+# if TempStr[-1] in ['f','F']:
+#     C = (eval(TempStr[0:-1]) - 32)/1.8
+#     print("转换后的温度是{:.2f}C".format(C))
+# elif TempStr[-1] in ['c','C']:
+#     F = 1.8*eval(TempStr[0:-1]) + 32
+#     print("转换后的温度是{:.2f}F".format(F))
+# else:
+#     print("格式输入错误")
